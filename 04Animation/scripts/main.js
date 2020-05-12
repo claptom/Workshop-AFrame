3caea2b34c0da09feb136abd60cbb5cf15d27766
Niclas Schmidt <mail@niclas-schmidt.de>
2020-05-12 11:52:19 +0200

    04 - 7

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -1,13 +1,20 @@
 
 
  // changing individual properties with code and using setInterval
-var rotationSpeed = 0.1;
+var rotationSpeed = 0.01;
 var myOtherBox = document.getElementById('myOtherBox');
+var ring1 = document.getElementById('ring1');
+var ring2 = document.getElementById('ring2');
+var boxMiddle = document.getElementById('boxMiddle');
 
 function spin(){
 myOtherBox.object3D.rotation.x += rotationSpeed;
 myOtherBox.object3D.rotation.y += rotationSpeed;
 myOtherBox.object3D.rotation.z += rotationSpeed;
+ring1.object3D.rotation.x += rotationSpeed;
+ring2.object3D.rotation.y += rotationSpeed;
+boxMiddle.object3D.rotation.x += rotationSpeed;
+boxMiddle.object3D.rotation.y += rotationSpeed;
 console.log(myOtherBox.object3D.rotation.x);
  }
 
